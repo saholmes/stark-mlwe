a8e208798c21ccf2806da69736db575f85cecce7
saholmes <41784476+saholmes@users.noreply.github.com>
2025-10-05 18:37:01 +0100

    completed initial proof system
    This is the completed initial proof system

--- target/debug/deps/commitment-18ef10030a71ac85.d
+++
@@ -1,7 +0,0 @@
-/Volumes/SAHexternal 1/Documents/stark-mlwe/target/debug/deps/libcommitment-18ef10030a71ac85.rmeta: crates/commitment/src/lib.rs
-
-/Volumes/SAHexternal 1/Documents/stark-mlwe/target/debug/deps/libcommitment-18ef10030a71ac85.rlib: crates/commitment/src/lib.rs
-
-/Volumes/SAHexternal 1/Documents/stark-mlwe/target/debug/deps/commitment-18ef10030a71ac85.d: crates/commitment/src/lib.rs
-
-crates/commitment/src/lib.rs:
--- target/debug/deps/transcript-bcef5369353a8006.d
+++
@@ -1,7 +0,0 @@
-/Volumes/SAHexternal 1/Documents/stark-mlwe/target/debug/deps/libtranscript-bcef5369353a8006.rmeta: crates/transcript/src/lib.rs
-
-/Volumes/SAHexternal 1/Documents/stark-mlwe/target/debug/deps/libtranscript-bcef5369353a8006.rlib: crates/transcript/src/lib.rs
-
-/Volumes/SAHexternal 1/Documents/stark-mlwe/target/debug/deps/transcript-bcef5369353a8006.d: crates/transcript/src/lib.rs
-
-crates/transcript/src/lib.rs:
